b937f2cd246ceece8a3004c29edf600f832be9ab
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-21 13:24:54 +0100

    stylovanie, cast notu sa zobrazi po pridani

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
   <div id="app">
     <div id="notes">
         <div id="first-note" class="first-note-cl">
-            <div id="blue-top" class="blue-top-cl"><button id="svg-delete-button" class="svg-delete-button-cl"><img src="./assets/add-note.svg" alt="add" class="svg-delete"></button></div>
-            <textarea id="note" class="note-cl"></textarea>
+            <div id="blue-top" class="blue-top-cl bg-blue-600 w-3/12 h-[50px] flex justify-end rounded-t-[12px]"><button id="svg-delete-button" class="svg-delete-button-cl h-[40px] w-[40px]  mt-[5px] mr-[7px]"><img src="./assets/add-note.svg" alt="add" class="svg-delete"></button></div>
+            <textarea id="note" class="note-cl bg-stone-600  w-3/12 min-h-[200px]	rounded-b-[12px]"></textarea>
         </div>
     </div>
 
     <div class="buttons">
-        <button id="but-reset" class="bg-blue-600 rounded-full h-[50px] w-[50px] fixed bottom-[25px] right-[120px]"><img src="./assets/reset.svg" class="ml-[6px]" alt="reset"></button>
-        <button id="but-add-note" @click="createNote()" class="bg-blue-600 rounded-full h-[85px] w-[85px] fixed bottom-[25px] right-[25px]"><img src="./assets/add-note.svg" class="ml-[14px]" alt="add"></button>
-        <button id="but-color-change" class="bg-blue-600 rounded-full h-[50px] w-[50px] fixed bottom-[120px] right-[25px]"><img src="./assets/horiz-dots.svg" class="ml-[8px]" alt="color"></button>
+        <button class="bg-blue-600 rounded-full h-[50px] w-[50px] fixed bottom-[25px] right-[120px]"><img src="./assets/reset.svg" class="ml-[6px]" alt="reset"></button>
+        <button @click="createNote()" class="bg-blue-600 rounded-full h-[85px] w-[85px] fixed bottom-[25px] right-[25px]"><img src="./assets/add-note.svg" class="ml-[14px]" alt="add"></button>
+        <button class="bg-blue-600 rounded-full h-[50px] w-[50px] fixed bottom-[120px] right-[25px]"><img src="./assets/horiz-dots.svg" class="ml-[8px]" alt="color"></button>
     </div>
   </div>
 
@@ -23,9 +23,17 @@
     
     methods: {
       createNote() {
-        let div = document.createElement('div');
-        div.className = 'nie';
-        document.body.appendChild(div);
+        let newNote = document.createElement('div');
+        newNote.className = 'newNote';
+        document.body.appendChild(newNote);
+
+        let newNoteBluePart = document.createElement('div');
+        newNoteBluePart.className = 'blue-top-cl bg-blue-600 w-3/12 h-[50px] flex justify-end rounded-t-[12px]';
+        newNote.appendChild(newNoteBluePart);
+      },
+
+      deleteNote() {
+        
       }
     }
 
@@ -34,4 +42,9 @@
 </script>
 
 <style>
+
+  body {
+    padding: 10px;
+  }
+
 </style>
